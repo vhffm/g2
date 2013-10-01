29a3269340e6607036a95e7ddc2d9651d1d6d173
Volker Hoffmann <volker@cheleb.net>
2013-10-01 08:25:27 +0200

    angle conversion

--- Helpers/g2_helpers.py
+++ Helpers/g2_helpers.py
@@ -3,6 +3,8 @@ import numpy as np
 twopi = 2.0 * np.pi
 pihalf = np.pi / 2.0
 MEarth = 3.0024584e-6       # Solar Masses
+d2r = twopi / 360.0
+r2d = 360. / twopi
 
 def mkline(x1, y1, x2, y2):
     m = (y2 - y1) / (x2 - x1)
