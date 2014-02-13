ea53eb62394a59e3d828d8e04001423863627c77
Volker Hoffmann <volker@cheleb.net>
2014-02-13 10:19:24 +0100

    compute stats for mass above and below cutoff

--- Post/compute_stats.py
+++ Post/compute_stats.py
@@ -14,6 +14,15 @@ group.add_argument('--all', action='store_true', \
                    help="Aggregative Full Set of Snapshots.")
 args = parser.parse_args()
 
+# Masses in kg
+m_earth   = 5.972e24
+m_sun     = 1.99e30
+m_jupiter = 1.89e27
+
+# Cutoff Mass
+m_cutoff = 2.0e23 # kg
+m_cutoff = m_cutoff / m_sun
+
 # List of Directories
 # @todo Check for existence
 if sys.stdin.isatty():
@@ -45,6 +54,8 @@ if args.all:
 # Loop Dirs, Snaps, Particles
 tout = np.zeros_like(nsteps)
 mass = np.zeros([len(dirs),nsteps.shape[0]])
+mass_above_cutoff = np.zeros_like(mass)
+mass_below_cutoff = np.zeros_like(mass)
 npart = np.zeros([len(dirs),nsteps.shape[0]])
 for idir, dirchar in enumerate(dirs):
     print "// Processing Directory %s [%i/%i]" % (dirchar, idir+1, len(dirs))
@@ -59,6 +70,10 @@ for idir, dirchar in enumerate(dirs):
                 # Do not count Jupiter and Saturn
                 if particle.id != 2000 and particle.id != 2001:
                     mass[idir,istep] += particle.m
+                    if particle.m >= m_cutoff:
+                        mass_above_cutoff[idir,istep] += particle.m
+                    else:
+                        mass_below_cutoff[idir,istep] += particle.m
         except IOError:
             mass[idir,istep] = np.nan
 
@@ -69,6 +84,18 @@ mass_med = np.median(mass, axis=0)
 mass_std = np.std(mass, axis=0)
 mass_q25 = np.percentile(mass, 25, axis=0)
 mass_q75 = np.percentile(mass, 25, axis=0)
+
+mass_above_cutoff_avg = np.mean(mass_above_cutoff, axis=0)
+mass_above_cutoff_med = np.median(mass_above_cutoff, axis=0)
+mass_above_cutoff_std = np.std(mass_above_cutoff, axis=0)
+mass_above_cutoff_q25 = np.percentile(mass_above_cutoff, 25, axis=0)
+mass_above_cutoff_q75 = np.percentile(mass_above_cutoff, 25, axis=0)
+
+mass_below_cutoff_avg = np.mean(mass_below_cutoff, axis=0)
+mass_below_cutoff_med = np.median(mass_below_cutoff, axis=0)
+mass_below_cutoff_std = np.std(mass_below_cutoff, axis=0)
+mass_below_cutoff_q25 = np.percentile(mass_below_cutoff, 25, axis=0)
+mass_below_cutoff_q75 = np.percentile(mass_above_cutoff, 25, axis=0)
 
 # Process Particle Number
 print "// Computing Particle Number Stats"
@@ -83,6 +110,16 @@ print "// Saving"
 np.savez("Stats.npz", \
     mass = mass, npart = npart, tout = tout, \
     mass_avg = mass_avg, mass_med = mass_med, mass_std = mass_std, \
+    mass_above_cutoff_avg = mass_above_cutoff_avg, \
+    mass_above_cutoff_med = mass_above_cutoff_med, \
+    mass_above_cutoff_std = mass_above_cutoff_std, \
+    mass_above_cutoff_q25 = mass_above_cutoff_q25, \
+    mass_above_cutoff_q75 = mass_above_cutoff_q75, \
+    mass_below_cutoff_avg = mass_below_cutoff_avg, \
+    mass_below_cutoff_med = mass_below_cutoff_med, \
+    mass_below_cutoff_std = mass_below_cutoff_std, \
+    mass_below_cutoff_q25 = mass_below_cutoff_q25, \
+    mass_below_cutoff_q75 = mass_below_cutoff_q75, \
     npart_avg = npart_avg, npart_med = npart_med, npart_std = npart_std, \
     mass_q25 = mass_q25, mass_75 = mass_q75, \
     npart_q25 = npart_q25, npart_q75 = npart_q75 \
