5da2534ef3fe41371a33423cf59f02e49ff0bc0e
Volker Hoffmann <volker@cheleb.net>
2014-02-21 10:30:35 +0100

    convert to simple xchaos output (only separation matrix and time vector)

---
+++ Post/chaos_light.py
@@ -0,0 +1,28 @@
+"""
+Read Full XChaos.npz.
+Write Light XChaos.npz.
+"""
+
+import numpy as np
+import argparse
+
+# Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--infile', default='XChaos.npz', \
+                    help="Output File Name.")
+parser.add_argument('--outfile', default='XChaos_Light.npz', \
+                    help="Output File Name.")
+args = parser.parse_args()
+
+# Read
+print "// Reading %s" % (args.infile,)
+npz_in = np.load(args.infile)
+ds = npz_in["ds"]
+tt = npz_in["tt"]
+
+# Write
+print "// Writing %s" % (args.outfile,)
+np.savez(args.outfile, ds = ds, tt = tt)
+
+# Done
+print "// Done"
