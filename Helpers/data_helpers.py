1a5ae19a1707d6e9a09dfc8f1da289c6b70090bb
Volker Hoffmann <volker@cheleb.net>
2014-04-07 09:04:53 +0200

    add inner solar system data

---
+++ Helpers/data_helpers.py
@@ -0,0 +1,9 @@
+"""
+Some Data Helpers.
+"""
+
+import numpy as np
+
+# Inner Solar System
+solar_a = np.array([ 0.387098, 0.723327, 1.0, 1.523679 ])
+solar_m = np.array([ 3.3022e23, 4.8676e24, 5.97219e24, 6.4185e23 ])
