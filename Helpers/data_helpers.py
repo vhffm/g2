f46b09d9e1d8864e5fa66c886638b31ad33ba4b4
Volker Hoffmann <volker@cheleb.net>
2014-04-08 11:10:44 +0200

    clarify solar system units

--- Helpers/data_helpers.py
+++ Helpers/data_helpers.py
@@ -5,5 +5,7 @@ Some Data Helpers.
 import numpy as np
 
 # Inner Solar System
+# Semi-Major Axis = [AU]
+# Mass            = [kg]
 solar_a = np.array([ 0.387098, 0.723327, 1.0, 1.523679 ])
 solar_m = np.array([ 3.3022e23, 4.8676e24, 5.97219e24, 6.4185e23 ])
