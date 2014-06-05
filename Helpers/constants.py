c9248f6496db467e848d8e26beb87bb233998dc6
Volker Hoffmann <volker@cheleb.net>
2014-06-05 11:39:09 +0200

    constants

---
+++ Helpers/constants.py
@@ -0,0 +1,16 @@
+"""
+Constants and Conversions.
+"""
+
+import nunpy as np
+
+# Masses
+mearth = 5.97e24 # kg
+msun   = 1.99e24 # kg
+
+# Angle Conversions
+r2d = 180.0 / np.pi
+d2r = np.pi / 180.0
+
+# Distance Conversions
+au2km = 149597871.0 # km
