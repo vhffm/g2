7c2679ccbb27c6d4540c274c9ce796f6f7d9d2ba
Volker Hoffmann <volker@cheleb.net>
2014-06-13 10:52:36 +0200

    fit to geometric mean instead of particles (particles do steps; bad idea to fit to that)

--- Post/fits.py
+++ Post/fits.py
@@ -9,7 +9,7 @@ Read Metric Distance. Compute Different Fits.
 import numpy as np
 import argparse
 from scipy.optimize import curve_fit
-import scipy.stats as sps
+import scipy.stats.mstats as mstats
 
 # Define Fitting Functions
 def fit_func_exp(t, a, b, c):
@@ -31,79 +31,52 @@ print "// Reading %s" % args.infile
 npz = np.load(args.infile)
 tout = npz["tout"]
 rho2 = npz["rho2"]
-ttx = np.repeat([tout], rho2.shape[1], axis=0).T
+# ttx = np.repeat([tout], rho2.shape[1], axis=0).T
+ttx = tout
 rho = np.sqrt(rho2/rho2[0,:])
-
-# Init Arrays
-te01 = []
-te02 = []
-slope03 = []
-
-# Debug Info
-print "// Fitting %i Particles" % rho.shape[1]
+rho = mstats.gmean(rho, axis=1)
 
 # Fit 01
 print "// Fitting First Exponential"
-failed = 0
-for ipart in range(rho.shape[1]):
-    ttx_loc = ttx[:,ipart]
-    rho_loc = rho[:,ipart]
-    ttx_loc = ttx_loc[rho_loc<1.0e7]
-    rho_loc = rho_loc[rho_loc<1.0e7]
-    try:
-        fitParams01, _ = curve_fit(fit_func_exp, ttx_loc, rho_loc, \
-                                   p0 = [ 40000.0, 0.01, -10000.0 ])
-        te01.append(1.0/fitParams01[1])
-    except (RuntimeError, TypeError):
-        failed += 1
-        te01.append(np.nan)
-
-te01 = np.array(te01)
-print "!! Fits Failed -- %i/%i" % (failed, rho.shape[1])
-print "   Median E-Folding Time = %.2e" % sps.nanmedian(te01)
+ttx_loc = ttx[rho<1.0e7]
+rho_loc = rho[rho<1.0e7]
+try:
+    fitParams01, _ = curve_fit(fit_func_exp, ttx_loc, rho_loc, \
+                               p0 = [ 40000.0, 0.01, -10000.0 ])
+    te01 = 1.0/fitParams01[1]
+except (RuntimeError, TypeError):
+    "!! Fit Failed"
+    te01 = np.nan
+print "   E-Folding Time = %.2e" % te01
 
 # Fit 02
 print "// Fitting Second Exponential"
-failed = 0
-for ipart in range(rho.shape[1]):
-    ttx_loc = ttx[:,ipart]
-    rho_loc = rho[:,ipart]
-    bool_01 = rho_loc>1.0e7
-    bool_02 = rho_loc<=1.0e11
-    bool_cb = np.logical_and(bool_01, bool_02)
-    ttx_loc = ttx_loc[bool_cb]
-    rho_loc = rho_loc[bool_cb]
-    try:
-        fitParams02, _ = curve_fit(fit_func_exp, ttx_loc, rho_loc, \
-                                   p0 = [ 4.0e9, 0.01, -1.0e10 ])
-        te02.append(1.0/fitParams02[1])
-    except (RuntimeError, TypeError):
-        failed += 1
-        te02.append(np.nan)
-
-te02 = np.array(te02)
-print "!! Fits Failed -- %i/%i" % (failed, rho.shape[1])
-print "   Median E-Folding Time = %.2e" % sps.nanmedian(te02)
+bool_01 = rho>1.0e7
+bool_02 = rho<=1.0e11
+bool_cb = np.logical_and(bool_01, bool_02)
+ttx_loc = ttx[bool_cb]
+rho_loc = rho[bool_cb]
+try:
+    fitParams02, _ = curve_fit(fit_func_exp, ttx_loc, rho_loc, \
+                               p0 = [ 40000.0, 0.01, -10000.0 ])
+    te02 = 1.0/fitParams02[1]
+except (RuntimeError, TypeError):
+    "!! Fit Failed"
+    te02 = np.nan
+print "   E-Folding Time = %.2e" % te02
 
 # Fit 03
 print "// Fitting Power Law"
-failed = 0
-for ipart in range(rho.shape[1]):
-    ttx_loc = ttx[:,ipart]
-    rho_loc = rho[:,ipart]
-    ttx_loc = ttx_loc[rho_loc>1.0e11]
-    rho_loc = rho_loc[rho_loc>1.0e11]
-    try:
-        fitParams03, _ = curve_fit(fit_func_pow, ttx_loc, rho_loc, \
-                                   p0 = [ 1.0e12, 0.3, -8.0e12 ])
-        slope03.append(fitParams03[1])
-    except (RuntimeError, TypeError):
-        failed += 1
-        slope03.append(np.nan)
-
-slope03 = np.array(slope03)
-print "!! Fits Failed -- %i/%i" % (failed, rho.shape[1])
-print "   Median Slope = %.2e" % sps.nanmedian(slope03)
+ttx_loc = ttx[rho>1.0e11]
+rho_loc = rho[rho>1.0e11]
+try:
+    fitParams03, _ = curve_fit(fit_func_pow, ttx_loc, rho_loc, \
+                               p0 = [ 40000.0, 0.01, -10000.0 ])
+    slope03 = 1.0/fitParams01[1]
+except (RuntimeError, TypeError):
+    "!! Fit Failed"
+    slopr03 = np.nan
+print "   Slope = %.2e" % slope03
 
 # Write
 print "// Writing %s" % args.outfile
