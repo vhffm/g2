9ca4f7f610410a6306a40cb0df0546655141d04b
Volker Hoffmann <volker@cheleb.net>
2014-10-15 08:23:46 +0200

    [py] filter/bin/save distance ratio by semi-major axis

--- Post/distance_means.py
+++ Post/distance_means.py
@@ -11,6 +11,8 @@ import scipy.stats.mstats as mstats
 parser = argparse.ArgumentParser()
 parser.add_argument('--outfile', default='Distances_Means.npz', \
                     help="Output File Name.")
+parser.add_argument('--semi-major-axis', action='store_true', \
+                    help="Filter/Bin/Save in Semi-Major Axis")
 args = parser.parse_args()
 
 # List of Directories
@@ -30,12 +32,14 @@ first = True
 for file in files:
     npz = np.load(file)
     if first:
+        a1 = npz["a1"]
         rho2 = npz["rho2"]
         dv2 = npz["dv2"]
         ds2 = npz["ds2"]
         tout = npz["tout"]
         first = False
     else:
+        a1 = np.concatenate((a1, npz["a1"]), axis=1)
         rho2 = np.concatenate((rho2, npz["rho2"]), axis=1)
         dv2 = np.concatenate((dv2, npz["dv2"]), axis=1)
         ds2 = np.concatenate((ds2, npz["ds2"]), axis=1)
@@ -52,12 +56,46 @@ rho2_mean = mstats.gmean(ratio_rho2, axis=1)
 dv2_mean = mstats.gmean(ratio_dv2, axis=1)
 ds2_mean = mstats.gmean(ratio_ds2, axis=1)
 
+# Filter/Bin Semi-Major Axis
+print "// Filtering Distances. Be Patient."
+if args.semi-major-axis:
+
+    rho2_means_a = []
+    for ii in [ 0, 1, 2, 3 ]:
+
+        print "   %s" % tags[ii]
+        
+        # Copy, Compute Ratio
+        a1_loc = a1.copy()
+        ratio_rho2_loc = ratio_rho2.copy()
+        
+        # Set Filters
+        if ii == 0:
+            mybool = a1_loc<1.0
+        elif ii == 1:
+            mybool = np.logical_and(a1_loc>=1.0, a1_loc<2.0) 
+        elif ii == 2:
+            mybool = np.logical_and(a1_loc>=2.0, a1_loc<3.0) 
+        elif ii == 3:
+            mybool = a1_loc>=3.0
+
+        # Apply Filters
+        ratio_rho2_loc[~mybool] = np.nan
+        ratio_rho2_loc = np.ma.masked_invalid(ratio_rho2_loc)
+        
+        # Mean
+        rho2_mean_loc = mstats.gmean(ratio_rho2_loc, axis=1)
+        
+        # Append
+        rho2_means_a.append(rho2_mean_loc)
+
 # Save
 print "// Saving Files"
 np.savez(args.outfile, tout = tout, \
          precision = npz["precision"][()], \
-         version = int(npz["version"][()]), \
+         version = int(npz["version"][()])+1, \
          rho2_mean = rho2_mean, \
+         rho2_means_a = rho2_means_a, \
          ds2_mean = ds2_mean, dv2_mean = dv2_mean )
 
 # Done
